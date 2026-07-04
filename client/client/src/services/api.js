a47e130e8dbf5555ce3214078acd47c83bc2f135
Darshan Kumbhar <darshankgithub@gmail.com>
2026-07-04 15:20:54 +0530

    Fix API base URL by removing extra slash

--- client/client/src/services/api.js
+++ client/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smarterp-pvj9.onrender.com//api",
+  baseURL: "https://smarterp-pvj9.onrender.com/api",
 });
 
 export default API;
